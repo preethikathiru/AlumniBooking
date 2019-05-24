3a89c8249b533718f0712c7dd7239cb8e57291df
arun <preethikathiru05@gmail.com>
2019-05-24 22:07:41 +0530

    accept booking with edit api, basic signup and login completed

--- app.js
+++ app.js
@@ -31,9 +31,78 @@ var studentSchema = new mongoose.Schema({
 });
 var Studentmodel = mongoose.model("student", studentSchema);
 
+var userSchema = new mongoose.Schema({
+    Username: {
+        type: String,
+        unique: true
+    },
+    Password: String
+});
+var Usermodel = mongoose.model("user", userSchema);
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/getbookinglist", (req, res) => {
+    res.sendFile(__dirname + "/alumni.html")
+})
+
+app.get("/signup", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/login.html")
+})
+
+app.post("/adduser", (req, res) => {
+    var userjson = {
+        Username: req.body.Username,
+        Password: req.body.Password
+    }
+    UserObject = new Usermodel(userjson)
+    UserObject.save()
+        .then(item => {
+            console.log(item)
+            res.send(item)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+});
+
+app.post("/validateuser", (req, res) => {
+    console.log()
+    Usermodel.findOne({ Username: req.body.Username })
+        .then(item => {
+            console.log(item, 'from db')
+            console.log(req.body, 'request')
+            if (item && item.Password === req.body.Password) {
+                console.log(item, 'logged in successfully')
+                res.send({
+                    success: true,
+                    data:item
+                })
+                //res.send('success')
+            }
+            else {
+                console.log('wrongly entered')
+                //res.status(req.body,'200')
+                res.send({
+                    success: false,
+                    data:item
+                })
+            }
+        })
+        .catch(err => {
+            res.send({
+                success: false,
+                error:'Db error'
+            })
+        })
+});
+
 
 app.get("/getalumni", (req, res) => {
     Studentmodel.find({ passed_out: true })
@@ -43,7 +112,7 @@ app.get("/getalumni", (req, res) => {
         .catch(err => {
             console.log(err)
         })
-})
+});
 
 app.post("/bookslot", (req, res) => {
     console.log(req.body);
@@ -72,18 +141,23 @@ app.get("/getbookings", (req, res) => {
 
 app.post("/editbookings", (req, res) => {
     console.log('hello')
+    console.log(req.body._id)
     Bookingmodel.findOne({ _id: req.body._id })
-        .then(function (booking) {
-            booking.Accepted = true
-            booking.save()
+        .then(function (item) {
+            console.log(req.body.Accepted)
+            item.Accepted = req.body.Accepted
+            item.save()
                 .then(item => {
-                    res.send(booking)
+                    res.send(item)
+                    console.log(item)
                 })
         })
         .catch(err => {
             console.log(err)
         })
 })
+
+
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
